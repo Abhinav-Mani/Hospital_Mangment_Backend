30d8194c739da0415cf787e8a6f004526486b6a6
Abhinav Mani <abhinav1946@gmail.com>
2020-03-01 14:36:38 +0530

    Auth MiddleWare Created

--- routes/Receptionist.js
+++ routes/Receptionist.js
@@ -1,10 +1,12 @@
 const express=require("express");
 
+const authCheck=require("../util/auth_check");
+
 const route=express.Router();
 
 const controler=require("../controllers/receptionist")
 
-route.get ("/",controler.receptionist_get);
+route.get ("/",authCheck,controler.receptionist_get);
 route.post("/",controler.SignUP);
 route.post("/signin",controler.SignIn);
 
