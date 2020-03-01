c066847d9dda38b0f8e8fbca0f18c14aa6ee61f1
Abhinav Mani <abhinav1946@gmail.com>
2020-03-01 23:15:39 +0530

    Pharmacist Route Added

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app=express();
 
 const receptionist=require("./routes/Receptionist");
 const doctor=require("./routes/Doctor");
+const pharmacist=require("./routes/Pharmacist");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/receptionist",receptionist);
 app.use("/doctor",doctor);
+app.use("/pharmacist",pharmacist);
 
 app.get("*",(req,res)=>{
     res.send("<h1>Hello World</h1>")
