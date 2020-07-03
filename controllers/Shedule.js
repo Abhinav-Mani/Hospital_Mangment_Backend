d2706fd3d603e3ee2dd33d65f8310cb7be943110
Abhinav Mani <abhinav1946@gmail.com>
2020-07-03 13:29:31 +0530

    Scheduling Featured Improved

--- controllers/Shedule.js
+++ controllers/Shedule.js
@@ -6,21 +6,36 @@ const pool=require("../util/database");
 
 exports.Schedule=(req,res)=>{
     let id=req.body.id;
+    let doctor=req.body.doctor;
+    let date =req.body.date;
     setSchedule();
     async function setSchedule(){
         let connection;
         try{
             connection=await (await pool).getConnection();
-            let result=await connection.execute("SELECT * FROM Schedule where trunc(SCHEDULE_TIME)=trunc(sysdate)");
-            let rowCount=result.rows.length;
+            let result;
+            let rowCount;
+            result=await connection.execute("SELECT * FROM PATIENT WHERE PATIENT_ID=(:1)",[id]);
+            rowCount=result.rows.length;
+            if(rowCount<1){
+                return res.send({Message:"No Such Patient Exists"});
+            }
+            result=await connection.execute("SELECT * FROM Schedule where trunc(SCHEDULE_DATE)=trunc(TO_DATE((:1),'YYYY-MM-DD')) AND PATIENT_ID=((:2))",[date,id]);
+            rowCount=result.rows.length;
+            if(rowCount>=1){
+                return res.send({Message:"You have already Registered"});
+            }
+            result=await connection.execute("SELECT * FROM Schedule where trunc(SCHEDULE_DATE)=trunc(TO_DATE((:1),'YYYY-MM-DD')) AND Doctor_id=((:2))",[date,doctor]);
+            rowCount=result.rows.length;
             if(rowCount>=process.env.MAX_NUMBER){
-                return res.send({status:"DATE FULL"});
+                return res.send({Message:"Date Full"});
             }
             rowCount++;
-            await connection.execute("INSERT INTO Schedule (QUEUE_NO,PATIENT_ID) VALUES ((:1),(:2))",[rowCount,id]);
+            await connection
+            .execute("INSERT INTO Schedule (QUEUE_NO,PATIENT_ID,Doctor_id,SCHEDULE_DATE) VALUES ((:1),(:2),(:3),trunc(TO_DATE((:4),'YYYY-MM-DD')))",[rowCount,id,doctor,date]);
             await connection.commit();
             res.status(200);
-            return res.json({queueNo:rowCount});
+            return res.json({Message:`Your Token no for day ${date} is ${rowCount}`});
         }catch(err){
             res.status(500);
             return res.json(err);
