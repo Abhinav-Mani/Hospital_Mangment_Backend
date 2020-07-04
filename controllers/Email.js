cc75285a2474c6fe4d69114fab6dd5086f28142d
Abhinav Mani <abhinav1946@gmail.com>
2020-07-04 20:10:31 +0530

    Mailing Controller Working

--- controllers/Email.js
+++ controllers/Email.js
@@ -1,5 +1,17 @@
 const mailer=require("../util/Mailer");
 
 module.exports.MAIL=(req,res)=>{
-    res.send("Mailing");
+    let email=req.body.email;
+    let message=req.body.message;
+    mailEmail();
+    async function mailEmail(){
+        try{
+            await mailer.sendAppointmentDetails(email,message)
+            res.send({message:"Email Sent"});
+            res.status(200);
+        }catch(error){
+            res.send({error});
+            res.status(500);
+        }
+    }
 }
