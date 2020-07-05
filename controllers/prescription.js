31d660554337ea82de256eecdee16a080927773e
Abhinav Mani <abhinav1946@gmail.com>
2020-07-05 12:45:55 +0530

    Prescription Controller added

--- controllers/prescription.js
+++ controllers/prescription.js
@@ -1,5 +1,23 @@
 const pool=require("../util/database");
 
 exports.ADD_PRESCRIPTION=(req,res)=>{
-    res.send("Prescription Route");
+    let list=req.body.list;
+    let id=req.body.id;
+    addPrescription();
+    async function addPrescription(med){
+        try{
+            let connection=await (await pool).getConnection();
+            console.log("connection set");
+            console.log(list);
+            for(let i=0;i<list.length;i++){
+                med=list[i];
+                await connection.execute("INSERT INTO prescription VALUES ((:1),(:2),(:3))",[id,med,0]);
+            }
+            res.send("sucess");
+        }catch(err){
+            console.log(err);
+            res.send({message:"failed"});
+        }
+    }
+    //res.send("Prescription Route");
 }
